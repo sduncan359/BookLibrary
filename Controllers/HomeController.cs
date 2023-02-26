4d650c8d1c0b1c58862068a54e7438f8f51c56be
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-02-26 12:34:05 -0800

    Add database populate.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using BookLibrary.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
-using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BookLibrary.Controllers
 {
@@ -19,64 +19,21 @@ namespace BookLibrary.Controllers
         public IActionResult Index()
         {
             return View();
-        }    
+        }
+
+        [Authorize()]
+        public ContentResult PopulateDB()
+        {             
+            //ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            //DataMethods.GetUserID(claimsIdentity);
+            DataMethods.PopulateBookDatabase();
+            return base.Content("<div>Database populate complete</div><br /><a href=\"/Home\">Home</a>", "text/html");        
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private string GetUserID()
-        {
-            string userIdValue = "";
-
-            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
-            if (claimsIdentity != null)
-            {
-                var userIdClaim = claimsIdentity.Claims
-                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
-
-                if (userIdClaim != null)
-                {
-                    userIdValue = (string)userIdClaim.Value;
-                }
-            }
-
-            return userIdValue;
-        }
-
-        private void PopulateBookDatabase()
-        {
-            Regex regx = new Regex("," + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-            StreamReader sr = new StreamReader(@"c:\work\book.csv");
-            using (var db = new BookContext())
-            {
-                string? line = sr.ReadLine();
-                do
-                {
-                    string[] cols = regx.Split(line);
-                    if (cols[0] != "Name" || cols[1] != "Author")
-                    {
-                        BookModel book = new BookModel();
-                        book.Name = cols[0];
-                        book.Author = cols[1];
-                        book.UserRating = Convert.ToDecimal(cols[2]);
-                        book.Reviews = Convert.ToInt32(cols[3]);
-                        book.Price = Convert.ToDecimal(cols[4]);
-                        book.Year = Convert.ToInt32(cols[5]);
-                        book.Genre = cols[6];
-
-                        db.Add(book);
-                        db.SaveChanges();
-                    }
-
-                    line = sr.ReadLine();
-                }
-                while (line != null);
-            }
-            sr.Close();
-            sr.Dispose();
-        }
+        }              
     }
 }
---
+++ Data/DataMethods.cs
@@ -0,0 +1,65 @@
+﻿using BookLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace BookLibrary.Data
+{
+    public class DataMethods
+    {      
+        // pass in User.Identity as ClaimsIdentity (or cast it)
+        public static string GetUserID(ClaimsIdentity claimsIdentity)
+        {
+            string userIdValue = "";
+            
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    userIdValue = (string)userIdClaim.Value;
+                }
+            }
+
+            return userIdValue;
+        }
+
+        // populate book database
+        public static void PopulateBookDatabase()
+        {
+            Regex regx = new Regex("," + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+            string appDataPath = Path.Combine(Directory.GetCurrentDirectory(), "App_Data");
+            StreamReader sr = new StreamReader(Path.Combine(appDataPath, "book.csv"));
+            using (var db = new BookContext())
+            {
+                db.RemoveRange(db.Books);
+                string? line = sr.ReadLine();
+                do
+                {
+                    string[] cols = regx.Split(line);
+                    if (cols[0] != "Name" || cols[1] != "Author")
+                    {
+                        BookModel book = new BookModel();
+                        book.Name = cols[0];
+                        book.Author = cols[1];
+                        book.UserRating = Convert.ToDecimal(cols[2]);
+                        book.Reviews = Convert.ToInt32(cols[3]);
+                        book.Price = Convert.ToDecimal(cols[4]);
+                        book.Year = Convert.ToInt32(cols[5]);
+                        book.Genre = cols[6];
+
+                        db.Add(book);
+                        db.SaveChanges();
+                    }
+
+                    line = sr.ReadLine();
+                }
+                while (line != null);
+            }
+            sr.Close();
+            sr.Dispose();
+        }
+    }
+}
