8282e2e6400442636e6f0d3988b3ddd4b9816376
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-02-26 17:03:43 -0800

    add code except for paging and error handling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,15 +17,60 @@ namespace BookLibrary.Controllers
         }
 
         public IActionResult Index()
-        {
+        {            
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            string userId = DataMethods.GetUserID(claimsIdentity);
+            TempData["numBooksCheckedOut"] = DataMethods.GetNumberOfBooksCheckedOut(userId);
+            TempData["userid"] = userId;
+            TempData["books"] = DataMethods.ShowAllBooks(1);            
+
             return View();
         }
 
-        [Authorize()]
+        [HttpPost]
+        public IActionResult SearchBooks(string searchTerm, string searchColumn)
+        {
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            string userId = DataMethods.GetUserID(claimsIdentity);
+            TempData["numBooksCheckedOut"] = DataMethods.GetNumberOfBooksCheckedOut(userId);
+            TempData["userid"] = userId;
+            TempData["books"] = DataMethods.ShowBooksForSearch(searchTerm, searchColumn);            
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult CheckOutBook(BookModel book)
+        {
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            string userId = DataMethods.GetUserID(claimsIdentity);            
+            DataMethods.CheckOutBookForUser(book, userId);
+            TempData["numBooksCheckedOut"] = DataMethods.GetNumberOfBooksCheckedOut(userId);
+            TempData["books"] = DataMethods.ShowAllBooks(1);
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult CheckInBook(BookModel book)
+        {
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            string userId = DataMethods.GetUserID(claimsIdentity);
+            DataMethods.CheckInBookForUser(book, userId);
+            TempData["numBooksCheckedOut"] = DataMethods.GetNumberOfBooksCheckedOut(userId);
+            TempData["books"] = DataMethods.ShowAllBooks(1);
+
+            return View("Index");
+        }
+
+        /// <summary>
+        /// Home\Populate wipes the books table and then re-populates it from App_Data\books.csv
+        /// Just here to make it easy to rebuild the database table without having to import data with
+        /// another tool. Also good place to test other database functions.
+        /// </summary>
+        /// <returns>Just a message to say that the method is done</returns>       
         public ContentResult PopulateDB()
-        {             
-            //ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
-            //DataMethods.GetUserID(claimsIdentity);
+        {                         
             DataMethods.PopulateBookDatabase();
             return base.Content("<div>Database populate complete</div><br /><a href=\"/Home\">Home</a>", "text/html");        
         }
--- Data/DataMethods.cs
+++ Data/DataMethods.cs
@@ -1,12 +1,98 @@
 ﻿using BookLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 using System.Security.Claims;
 using System.Text.RegularExpressions;
 
 namespace BookLibrary.Data
 {
     public class DataMethods
-    {      
+    {
+        private const int c_PageSize = 25;
+
+        // pageNumber is 1-based
+        public static List<BookModel> ShowAllBooks(int pageNumber)
+        {
+            List<BookModel> books = new List<BookModel>();
+
+            using (var db = new BookContext())
+            {
+                books = db.Books.OrderBy(b => b.Name).Skip(pageNumber - 1).Take(c_PageSize).ToList();
+            }
+
+            return books;
+        }
+
+        public static void CheckOutBookForUser(BookModel book, string userId)
+        {
+            using (var db = new BookContext())
+            {
+                var UpdateBook = db.Books.Where(b => b.Id == book.Id && b.CheckOutUserId == null).FirstOrDefault();
+
+                if (UpdateBook != null)
+                {
+                    UpdateBook.CheckOutUserId = userId;
+                    db.SaveChanges();
+                }                
+            }
+        }
+
+        public static void CheckInBookForUser(BookModel book, string userId)
+        {
+            using (var db = new BookContext())
+            {
+                var UpdateBook = db.Books.Where(b => b.Id == book.Id).FirstOrDefault();
+
+                if (UpdateBook != null)
+                {
+                    UpdateBook.CheckOutUserId = null;
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public static int GetNumberOfBooksCheckedOut(string userId)
+        {
+            int numBooks = 0;
+
+            using (var db = new BookContext())
+            {               
+                numBooks = db.Books.Count(b => b.CheckOutUserId == userId);
+            }
+
+            return numBooks;
+        }
+
+        public static List<BookModel> ShowBooksForSearch(string searchTerm, string searchColumn, int pageNumber = 1)
+        {
+            List<BookModel> books = new List<BookModel>();
+
+            if (searchTerm.Trim().Length > 0)
+            {
+                using (var db = new BookContext())
+                {
+                    switch (searchColumn)
+                    {
+                        case "Author":
+                            books = db.Books.Where(b => b.Author.ToLower().Contains(searchTerm.Trim().ToLower())).OrderBy(b => b.Name).Skip(pageNumber - 1).Take(c_PageSize).ToList();
+                            break;
+                        case "Year":
+                            int result = -1;
+                            if (Int32.TryParse(searchTerm, out result))
+                            {
+                                books = db.Books.Where(b => b.Year == result).OrderBy(b => b.Name).Skip(pageNumber - 1).Take(c_PageSize).ToList();
+                            }
+                            break;
+                        default: // Title
+                            books = db.Books.Where(b => b.Name.ToLower().Contains(searchTerm.Trim().ToLower())).OrderBy(b => b.Name).Skip(pageNumber - 1).Take(c_PageSize).ToList();
+                            break;
+                    }
+                }
+            }
+
+            return books;
+        }
+
         // pass in User.Identity as ClaimsIdentity (or cast it)
         public static string GetUserID(ClaimsIdentity claimsIdentity)
         {
@@ -24,7 +110,7 @@ namespace BookLibrary.Data
             }
 
             return userIdValue;
-        }
+        }      
 
         // populate book database
         public static void PopulateBookDatabase()
@@ -42,7 +128,8 @@ namespace BookLibrary.Data
                     if (cols[0] != "Name" || cols[1] != "Author")
                     {
                         BookModel book = new BookModel();
-                        book.Name = cols[0];
+                        string bookName = cols[0];
+                        book.Name = Regex.Replace(bookName.Trim(), "^\"|\"$", "");                        
                         book.Author = cols[1];
                         book.UserRating = Convert.ToDecimal(cols[2]);
                         book.Reviews = Convert.ToInt32(cols[3]);
